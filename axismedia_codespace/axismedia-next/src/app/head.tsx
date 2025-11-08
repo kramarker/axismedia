8021cbdfc9403ffbf81a4a6ce5f9f571158059b3
kramarker <meantar2@illinois.edu>
2025-11-08 20:34:40 +0000

    Add GSC verification

---
+++ axismedia_codespace/axismedia-next/src/app/head.tsx
@@ -0,0 +1,11 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="NAE-Gcdo8348mg8-KbZJIhhbyWWFV1SWHj78XuTAA_w"
+      />
+    </>
+  );
+}
